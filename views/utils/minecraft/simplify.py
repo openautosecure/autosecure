b5d938623fc161ec5b5f15af646817fce9236d11
salomao31 <salomaoiogourte31@gmail.com>
2026-03-18 18:58:46 +0000

    Removed Hypixel API

--- views/utils/minecraft/simplify.py
+++ views/utils/minecraft/simplify.py
@@ -1,4 +1,7 @@
 def simplify(amount: float) -> str:
+    if not amount:
+        return 0
+    
     amount = float(amount)
     if amount >= 1_000_000_000_000:
         return f"{amount / 1_000_000_000_000:.2f} Trillion"

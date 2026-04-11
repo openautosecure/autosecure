d5046f45cc9d966d488feecb6ffa04dbce61f54f
openautosecure <dragonite70m@gmail.com>
2026-04-11 10:51:30 +0100

    Fix hypixel stats for statless setups

--- views/utils/securing/getT.py
+++ views/utils/securing/getT.py
@@ -3,7 +3,7 @@ import re
 
 async def getT(session: httpx.AsyncClient):
     # Neccessary var for a post request cant really explain it literally
-    
+
     fetchT = await session.get(
         url = "https://login.live.com/login.srf?wa=wsignin1.0&rpsnv=21&ct=1708978285&rver=7.5.2156.0&wp=SA_20MIN&wreply=https://account.live.com/proofs/Add?apt=2&uaid=0637740e739c48f6bf118445d579a786&lc=1033&id=38936&mkt=en-US&uaid=0637740e739c48f6bf118445d579a786",
         follow_redirects = False
@@ -17,6 +17,5 @@ async def getT(session: httpx.AsyncClient):
     if match:
         return match.group(1)
     
-    # Account needs to accept TOS to be fixed
     return None 
     

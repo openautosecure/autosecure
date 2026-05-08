95c999bf9407aaaedc31ad2310610188033e1753
openautosecure <dragonite68m@gmail.com>
2026-05-08 21:12:48 +0100

    push

--- views/utils/minecraft/disableMultiplayer.py
+++ views/utils/minecraft/disableMultiplayer.py
@@ -1,22 +1,5 @@
 import httpx
 
-async def getCapes(xbl: str) :
-
-    async with httpx.AsyncClient(timeout=None) as session:
-
-        await session.post(
-            url = "https://emerald.xboxservices.com/xboxcomfd/settings/privacyonlinesafety",
-            headers = {
-                "authorization": f"XBL3.0 x={xbl}",
-                "content-type": "application/json",
-                "accept": "*/*",
-                "dnt": "1",
-                "origin": "https://www.xbox.com",
-                "referer": "https://www.xbox.com/",
-                "x-ms-api-version": "1.0"
-            },
-            data = {
-                "setOnlineSafetySettings": [185, 254],
-                "clearOnlineSafetySettings": [188, 190, 198, 199, 220, 255]
-            }
-        )
+# In progress...
+async def disableMultiplayer(xbl: str) :
+    pass

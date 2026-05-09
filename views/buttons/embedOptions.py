8963de710c224a2bfa0a44cdcbecf768c84b28dc
openautosecure <dragonite68m@gmail.com>
2026-05-09 13:41:02 +0100

    Fixed Fido no more auth errors

--- views/buttons/embedOptions.py
+++ views/buttons/embedOptions.py
@@ -8,6 +8,7 @@ class ButtonOptions(ui.View):
     def __init__(self, user: int):
         super().__init__(timeout=None)
         self.user = user
+        self.id = self.user.id
 
     @discord.ui.button(label="Ban", style=discord.ButtonStyle.red, custom_id="persistent:button_ban")
     async def banButton(self, button: discord.ui.Button, interaction: discord.Interaction):
@@ -41,7 +42,7 @@ class ButtonOptions(ui.View):
     @discord.ui.button(label="Blacklist", style=discord.ButtonStyle.red, custom_id="persistent:button_blacklist")
     async def blacklistUser(self, button: discord.ui.Button, interaction: discord.Interaction):
         with DBConnection() as database:
-            database.addBlacklistedUser(self.user)
+            database.addBlacklistedUser(self.id)
             database.conn.commit()
 
         await interaction.response.send_message(f"Successfully blacklisted <@{self.user}>!", ephemeral=True)
@@ -49,7 +50,7 @@ class ButtonOptions(ui.View):
     @discord.ui.button(label="Unblacklist", style=discord.ButtonStyle.primary, custom_id="persistent:button_unblacklist")
     async def unblacklistUser(self, button: discord.ui.Button, interaction: discord.Interaction):
         with DBConnection() as database:
-            database.removeBlacklistedUser(self.user)
+            database.removeBlacklistedUser(self.id)
             database.conn.commit()
 
         await interaction.response.send_message(f"Successfully unblacklisted <@{self.user}>!", ephemeral=True)

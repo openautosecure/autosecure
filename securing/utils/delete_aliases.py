8ce45e0bdf0dc899d61261a32aa182c7a5029869
openautosecure <dragonite68m@gmail.com>
2026-06-10 15:48:54 +0100

    Remove Aliases

--- securing/utils/delete_aliases.py
+++ securing/utils/delete_aliases.py
@@ -9,14 +9,26 @@ async def delete_aliases(session: httpx.AsyncClient) -> None:
         headers={"Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8"},
         follow_redirects=True
     )
+
     canary = re.search(r'name="canary"\s+value="([^"]+)"', response.text)
-
-
-    # Remove Alias
-    await session.post(
-        url = "https://account.live.com/names/Manage",
-        headers = {
-            "Content-Type": "application/x-www-form-urlencoded",
-            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8"
-        }
+    aliases = re.findall(
+        r'class="aliasRemoveLink"[^>]*name="([^"]+)"[^>]*data-display="([^"]+)"',
+        response.text
     )
+
+    if aliases:
+        for alias in aliases:
+            # Remove Alias
+            await session.post(
+                url = "https://account.live.com/names/Manage",
+                headers = {
+                    "Content-Type": "application/x-www-form-urlencoded",
+                    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8"
+                },
+                data = {
+                    "canary": canary,
+                    "action": "RemoveAlias",
+                    "aliasName": alias[0],
+                    "displayName": alias[1]
+                }
+            )

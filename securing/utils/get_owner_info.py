cac4e20029f3c5224ba41b2c530fc01b4859e450
openautosecure <dragonite68m@gmail.com>
2026-06-24 13:19:50 +0100

    Fixes

--- securing/utils/get_owner_info.py
+++ securing/utils/get_owner_info.py
@@ -10,11 +10,10 @@ async def get_owner_info(session: httpx.AsyncClient, verification_token: str):
             headers = {
                 "Accept": "application/json, text/plain, */*",
                 "__RequestVerificationToken": verification_token,
-                "Correlation-Context": "v=1,ms.b.tel.market=en-US,ms.b.qos.rootOperationName=GLOBAL.PROFILE.PERSONALINFO.GETPERSONALINFO",
-                "Referer": "https://account.microsoft.com/profile?lang=en-US",
+                "Correlation-Context": "v=1,ms.b.tel.market=en-US,ms.b.qos.rootOperationName=GLOBAL.PROFILE.PERSONALINFO.GETPERSONALINFO"
             }
         )
-
+        print(getInfo.text)
         response = getInfo.json()
         print(getInfo.text)
         return {
